2c58d02f23b405eb34f954dc191c6c7511e3a1d4
jpetesdev <55292817+jpetesdev@users.noreply.github.com>
2023-02-14 19:08:33 -0600

    Create run.py

---
+++ run.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import os
+import requests
+
+# set source dir for feedback file:
+src_dir = "/data/feedback/"
+#src_dir = "./review_files/"
+# capture list of files:
+files = os.listdir(src_dir)
+
+# function to read file lines into list:
+def readlines(file):
+    with open(src_dir + file) as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+# load feedback entries into dictionary:
+feedback = []
+keys = ["title", "name", "date", "feedback"]
+for file in files:
+    lines = readlines(file)
+    feedback.append(dict(zip(keys, lines)))
+print(feedback)
+
+# set host url:
+url = "http://104.197.51.223/feedback/"
+
+# post feedback entries:
+for entry in feedback:
+    response = requests.post(url, data=entry)
+    if response.ok:
+        print("loaded entry")
+    else:
+        print(f"load entry error: {response.status_code}")
